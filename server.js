189a950867abd8682c869b749e6af789e2b35bf6
Paulson Robert <paulsonr07@gmail.com>
2024-01-29 11:22:50 +0530

    Append new content in the file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 fs.readFile(path.join(__dirname, "Files", "msg.txt"), "utf8", (err, data) => {
   if (err) throw err;
-  console.log(data);
+  console.log("Read Complete!");
 });
 
 fs.writeFile(
@@ -11,6 +11,15 @@ fs.writeFile(
   "This is a newly created file.",
   (err) => {
     if (err) throw err;
+    console.log("Write Complete!");
+    fs.appendFile(
+      path.join(__dirname, "Files", "new.txt"),
+      "\n\n This is appended text!!",
+      (err) => {
+        if (err) throw err;
+        console.log("Append Complete!");
+      }
+    );
   }
 );
 
