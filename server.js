0b771e10f2eb8fc72dcbca1b3c3c75d35c4b76e6
Paulson Robert <paulsonr07@gmail.com>
2024-01-29 11:13:25 +0530

    Write new file

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ fs.readFile(path.join(__dirname, "Files", "msg.txt"), "utf8", (err, data) => {
   console.log(data);
 });
 
+fs.writeFile(
+  path.join(__dirname, "Files", "new.txt"),
+  "This is a newly created file.",
+  (err) => {
+    if (err) throw err;
+  }
+);
+
 process.on("uncaughtException", (err) => {
   console.error("There is an uncaught error : ", err);
   process.exit(1);
