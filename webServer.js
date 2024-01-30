0e24e156165c9291086f4a176a7c1e59b2102252
Paulson Robert <paulsonr07@gmail.com>
2024-01-30 12:52:19 +0530

    Web server listen and fetch html page via server and render in PORT 3000

---
+++ webServer.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+
+const webServer = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("index.html", (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.write("Something went wrong! ", err);
+    } else {
+      res.write(data);
+    }
+    res.end();
+  });
+});
+
+webServer.listen(PORT, (err) => {
+  if (err) console.error("Something went wrong! ", err);
+  else console.log("Listening on PORT: " + PORT);
+});
